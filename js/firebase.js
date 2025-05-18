0873f7f7f38e235a6ce1e56a89a03cf55932ab6d
frcarnage <vinitjha2712@gmail.com>
2025-05-18 20:49:56 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+// js/firebase.js
+
+// Your Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPzGXGIzSbDysU2COetHSeHTl_ZEuZx1Q",
+  authDomain: "nice-ae434.firebaseapp.com",
+  projectId: "nice-ae434",
+  storageBucket: "nice-ae434.appspot.com",
+  messagingSenderId: "880835929368",
+  appId: "1:880835929368:web:23baf239b8804f0c1a6a6d",
+  measurementId: "G-P9FQRZP2BF"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
